79c41dce0e5c8dfa4e9f41c40e9789783b6ca121
sheriefkmarakkar <muhammadsheriefkm@gmail.com>
2019-08-01 08:35:43 +0530

    üpdate

--- htmlcss/js.js
+++ htmlcss/js.js
@@ -11,11 +11,11 @@ $(document).ready(function(){
                console.log(data);
                
               var result="";
-              alert("hello");
+             
                for(var i in data['books'])
                {
-                  
-                var out="<div class='card mb-3' style='max-width: 540px;'><div class='row no-gutters'><div class='col-md-4'><img src='./ba.jpg' class='card-img' alt='...'></div><div class='col-md-8'><div class='card-body'><div class='card-header'>Book Name :"+data['books'][i].bname+"</div><div class='card-body'><strong><p class='card-text'>Auther :"+data['books'][i].auther+"</p></strong><p class='card-text'>Cat :"+data['books'][i].bcat+"</p><p class='card-text'>Discription :"+data['books'][i].bdis+"</p></div></div></div>";
+                 alert(); 
+                var out="<div class='card mb-3' style='max-width: 540px;'><div class='row no-gutters'><div class='col-md-4'><img src="+data['books'][i].url+" class='card-img' alt='...'></div><div class='col-md-8'><div class='card-body'><div class='card-header'>Book Name :"+data['books'][i].bname+"</div><div class='card-body'><strong><p class='card-text'>Auther :"+data['books'][i].auther+"</p></strong><p class='card-text'>Cat :"+data['books'][i].bcat+"</p><p class='card-text'>Discription :"+data['books'][i].bdis+"</p></div></div></div>";
                result+=out; 
             }
            
---
+++ htmlcss/news.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    
+    
+
+     
+   
+    $.ajax({
+        type:"GET",
+        url:"https://newsapi.org/v2/top-headlines?country=in&apiKey=e30a3e5c75134053815aded41d3e17bd",
+        success:function(data){
+            console.log(data);
+            
+           var result="";
+          
+            for(var i in data['articles'])
+            {
+           
+             var out="<div class='card border-primary mb-3' style='max-width: 1040px;'><div class='row no-gutters'><div class='col-md-4'><img src="+data['articles'][i].urlToImage+" class='card-img' alt='...'></div><div class='col-md-8'><div class='card-body'><div class='card-header'>"+data['articles'][i].title+"</div><div class='card-body'><strong><p class='card-text'>Auther :"+data['articles'][i].content+"</p></strong><p class='card-text'><small class='text-muted'>Published At :"+data['articles'][i].publishedAt+"</small></p></div></div></div>";
+            result+=out; 
+         }
+        
+       
+         $("#did").html(result);
+         
+          
+            
+            
+        }
+    });
+
+ 
+});
