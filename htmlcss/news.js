998540e4a2bd532c360a0fd66ab8ac226005e51c
Krishnadas1980 <abhiradh.krishna@gmail.com>
2019-08-01 16:37:19 +0530

    update

--- htmlcss/news.js
+++ htmlcss/news.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
             for(var i in data['articles'])
             {
            
-             var out="<div class='card border-primary mb-3' style='max-width: 1040px;'><div class='row no-gutters'><div class='col-md-4'><img src="+data['articles'][i].urlToImage+" class='card-img' alt='...'></div><div class='col-md-8'><div class='card-body'><div class='card-header'>"+data['articles'][i].title+"</div><div class='card-body'><strong><p class='card-text'>Auther :"+data['articles'][i].content+"</p></strong><p class='card-text'><small class='text-muted'>Published At :"+data['articles'][i].publishedAt+"</small></p></div></div></div>";
+             var out="<div class='card border-primary mb-3' style='max-width: 1040px;'><div class='row no-gutters'><div class='col-md-4'><img src="+data['articles'][i].urlToImage+" class='card-img' alt='...'></div><div class='col-md-8'><div class='card-body'><div class='card-header'>"+data['articles'][i].title+"</div><div class='card-body'><strong><p class='card-text'>Auther :"+data['articles'][i].content+"</p></strong><p class='card-text'><small class='text-muted'>Published At :"+data['articles'][i].publishedAt+"</small></p></div></div></div></div></div>";
             result+=out; 
          }
         
